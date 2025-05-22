66d169c03501ce7c55e5b0b7a1457fefa61d642e
Bhaskar <bhaskar@localhost.localdomain>
2025-05-22 20:12:13 +0530

    Testing 2.1

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Learner! This is a DevOps project."
+    return "Hello, World! This is a DevOps project from Bhaskar"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
