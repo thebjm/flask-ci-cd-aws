dcaacf9f65fc1f129eeb77def38975a2f60d5699
Super User <root@localhost.localdomain>
2025-05-22 17:50:33 +0530

    Correction Flask app

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, Bhaskar! This is a DevOps project."
+    return "Hello, World! This is a DevOps project."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
